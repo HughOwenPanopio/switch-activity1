085c93a4459a1a39b0999ba22a70568d7070b4b7
HughOwenPanopio <panopiohughowen@gmail.com>
2023-09-17 20:45:03 +0800

    enhanced with key pressed and animation

--- script.js
+++ script.js
@@ -1,44 +1,63 @@
-var carButtons = document.querySelectorAll("button").length;
+var carButtons = document.querySelectorAll(".car").length;
 console.log(carButtons);
 
 for (var i = 0; i < carButtons; i++) {
-    document.querySelectorAll("button")[i].addEventListener("click", function(){
-
+    document.querySelectorAll(".car")[i].addEventListener("click", function(){
     var cars = this.innerText;
-    console.log(cars);
-    switch (cars) {
-        case "challenger":
-            let challenger = new Audio("sounds/challenger.mp3");
-            challenger.play();
-            break;
-
-        case "p911":
-            let p911 = new Audio("sounds/911.mp3");
-            p911.play();
-            break;
-
-        case "m5":
-            let m5 = new Audio("sounds/m5.mp3");
-            m5.play();
-            break;
-
-        case "gtr":
-            let gtr = new Audio("sounds/gtr.mp3");
-            gtr.play();
-            break;
-
-        case "supra":
-            let supra = new Audio("sounds/supra.mp3");
-            supra.play();
-            break;
-
-        case "paul":
-            let paul = new Audio("sounds/paulwalker.mp3");
-            paul.play();
-            break;
-
-        default:
-            break;
-        }
+    buttonAnimation(cars)
+    playSound(cars)
     });
 }
+
+document.addEventListener("keydown", function(e){
+    console.log(e);
+    buttonAnimation(e.key)
+    playSound(e.key)
+});
+    // console.log(cars);
+    function playSound(key){
+        switch (key) {
+            case "c":
+                let challenger = new Audio("sounds/challenger.mp3");
+                challenger.play();
+                break;
+    
+            case "p":
+                let p911 = new Audio("sounds/911.mp3");
+                p911.play();
+                break;
+    
+            case "m":
+                let m5 = new Audio("sounds/m5.mp3");
+                m5.play();
+                break;
+    
+            case "g":
+                let gtr = new Audio("sounds/gtr.mp3");
+                gtr.play();
+                break;
+    
+            case "s":
+                let supra = new Audio("sounds/supra.mp3");
+                supra.play();
+                break;
+    
+            case "w":
+                let walker = new Audio("sounds/paulwalker.mp3");
+                walker.play();
+                break;
+    
+            default:
+                break;
+            }
+    }
+
+    function buttonAnimation(currentKey) {
+        var activeButton = document.querySelector("." + currentKey);
+
+        activeButton.classList.add("pressed");
+
+        setTimeout(function(){
+            activeButton.classList.remove("pressed");
+        }, 100);
+    }
